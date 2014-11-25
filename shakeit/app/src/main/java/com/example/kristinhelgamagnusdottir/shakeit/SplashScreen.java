7da949df4a2567d5e3db6cd9e4db04d74a242f3b
Emlinn <emilragnars@gmail.com>
2014-11-25 12:25:18 +0000

    Smá lagfæringar

--- shakeit/app/src/main/java/com/example/kristinhelgamagnusdottir/shakeit/SplashScreen.java
+++ shakeit/app/src/main/java/com/example/kristinhelgamagnusdottir/shakeit/SplashScreen.java
@@ -12,7 +12,7 @@ import android.os.Handler;
 
 public class SplashScreen extends Activity {
 
-    private static int SPLASH_TIME_OUT = 4000;
+    private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
